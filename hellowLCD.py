35845704cc7e427bac9b1831406097beaef8e808
mmosley7 <Marlowemosley22@gmail.com>
2018-03-19 18:40:41 +0000

    this is the finished product

--- hellowLCD.py
+++ hellowLCD.py
@@ -3,13 +3,38 @@
 # Kinglow
 
 import Adafruit_CharLCD as LCD
-
+import time
+import subprocess
 
 lcd = LCD.Adafruit_CharLCDPlate()
+IP = subprocess.check_output(['hostname','-I'])
+Name = subprocess.check_output(['hostname']).strip()
 
-
-displayText = "hello World"
+displaytext1 = Name
+displaytext = "hello World"
 
 lcd.clear()
-lcd.message(displayText)
+lcd.message(displaytext)
+
+refresh = True
+wrefresh = True
+while (True):
+        if lcd.is_pressed(LCD.SELECT):
+            if wrefresh:
+                lcd.clear()
+                lcd.set_backlight(1)
+                lcd.message(displaytext)
+                refresh = True
+                wrefresh = False
+
+
+        else:
+            if refresh:
+                lcd.clear()
+                lcd.set_backlight(1)
+                lcd.message(IP)
+                lcd.message(Name)
+                refresh = False
+                wrefresh = True
+            time.sleep(0.5)
 
